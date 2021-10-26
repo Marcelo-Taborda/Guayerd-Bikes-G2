65e3495bb819fc096761229c976432b9eda3264f
waskar7 <waskarmedina7@gmail.com>
2021-10-26 19:51:17 -0300

    Hice caso a Marce

--- js/contactos.js
+++ js/contactos.js
@@ -3,15 +3,19 @@ let aleatorio = Math.floor(Math.random() * 4) + 0;
 // const divcodigo = document.getElementById("codigo");
 function codigo() {
   const divcodigo = document.getElementById("codigo");
-
+  const divcodigo2 = document.getElementById("codigo2");
   if (localStorage.getItem("nombre")) {
+    divcodigo2.style.display = "flex";
     console.log(divcodigo);
-    divcodigo.innerText = "entro";
-    //   "Estimado " +
-    //   localStorage.getItem("nombre") +
-    //   ", ¡gracias por elegirnos! Le obsequiamos el codigo de " +
-    //   `${hotsale[aleatorio]}HOTSALE para obtener un ${hotsale[aleatorio]}% de descuento` +
-    //   " con su compra";
+    divcodigo.innerText =
+      "Estimado " +
+      localStorage.getItem("nombre") +
+      ", ¡gracias por elegirnos! Le obsequiamos el codigo de " +
+      `${hotsale[aleatorio]}HOTSALE para obtener un ${hotsale[aleatorio]}% de descuento` +
+      " con su compra";
+    setTimeout(() => {
+      divcodigo2.style.display = "none";
+    }, 15000);
   }
 }
 codigo();
