9e7d2e91f4711ad21f23a82d9a4ad1b23e8ab9e3
MarceloTaborda <marcelotaborda002@gmail.com>
2021-10-26 20:58:50 -0300

    bug nombre null home.js

--- js/home.js
+++ js/home.js
@@ -1,15 +1,17 @@
 function saludo() {
-    const fecha = new Date();
-    const hora = fecha.getHours();
-    console.log(hora)
-    if (hora >= 0 && hora < 6) {
-        alert("Buenas madrugadas, " + localStorage.getItem('nombre'));
-    } else if (hora >= 6 && hora < 12) {
-        alert("Buen dia, " + localStorage.getItem('nombre'));
-    } else if (hora >= 12 && hora < 19) {
-        alert("Buenas tardes, " + localStorage.getItem('nombre'));
-    } else {
-        alert("Buenas noches, " + localStorage.getItem('nombre'));
+    if (localStorage.setItem('nombre')) {
+        const fecha = new Date();
+        const hora = fecha.getHours();
+        console.log(hora)
+        if (hora >= 0 && hora < 6) {
+            alert("Buenas madrugadas, " + localStorage.getItem('nombre'));
+        } else if (hora >= 6 && hora < 12) {
+            alert("Buen dia, " + localStorage.getItem('nombre'));
+        } else if (hora >= 12 && hora < 19) {
+            alert("Buenas tardes, " + localStorage.getItem('nombre'));
+        } else {
+            alert("Buenas noches, " + localStorage.getItem('nombre'));
+        }
     }
 }
 saludo()
